1193cf14dcfec32dd726d08213413fd4f897c1bd
angDecoder <angshudas012@gmail.com>
2023-02-05 16:32:06 +0530

    carousel created

---
+++ src/assets/OnboardedTemples.js
@@ -0,0 +1,44 @@
+const temples = [
+    {
+        src : 'https://lh3.googleusercontent.com/p/AF1QipOjEne2Uu4SeIaik0vRS5pxLs3JzZwanslxEar6=s400-w400',
+        name : 'Badi Kali Ji Temple',
+        location : 'Lucknow'
+    },
+    {
+        src : "https://lh3.googleusercontent.com/p/AF1QipNcdtFhhChLIN7nZIk7PnD4tZYtTTXAGTG5MaPg=s500-w500",
+        name : 'Chamunda Devi Temple',
+        location : 'Kangra'
+    },
+    {
+        src : "https://lh3.googleusercontent.com/p/AF1QipPv1sEU9c0dIAGL4wadOBw8zZWIxiDsEYV2Pbd6=s500-w500",
+        name : "Shree Durgiana Temple",
+        location : "Amritsar"
+    },
+    {
+        src : 'https://lh3.googleusercontent.com/p/AF1QipMfMdBM57hsofMF1RTCIOBG6fRV7w3kB5T-zZCv=s500-w500',
+        name : 'Ma Vindhyavasini Mandir',
+        location : 'Mirzapur'
+    },
+    {
+        src :"https://lh3.googleusercontent.com/p/AF1QipO2ZpXfBqhUBB0KAs_feIxTYFJHYNXXuIxCpgvZ=s500-w500",
+        name : 'Tapkeshwar Mandir',
+        location : 'Dehradun'
+    },
+    {
+        src : "https://lh3.googleusercontent.com/places/ABKp1IofPG49m5s6IAn1m7Ky4cW0OJIFfba1OcowcVjOLMSIV2R0F3w8rOYyXl2pR3OwHyVe91gO7oqFc5sGZYppB7N5hk3z_EVc-yE=s500-w500",
+        name : 'Shaktipeeth Maa Baglamukhi Temple',
+        location : 'Kangra'
+    },
+    {
+        src : "https://lh3.googleusercontent.com/p/AF1QipPRsBqx7fHccE556IqVTsyCDmjw4JhFW0-nr-HA=s500-w500",
+        name : 'Jyotirlinga Ghushmeshwarnath(Ghuisarnath Dham)',
+        location : 'Pratapgarh'
+    },
+    {
+        src : "https://lh3.googleusercontent.com/p/AF1QipP7RW29K_nP78OUnSI4dP-x4qox8WXk4uxUfnGq=s500-w500",
+        name : 'Shri Vrindavan Chandrodaya Temple',
+        location : 'Mathura'
+    }
+];
+
+export default temples;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react';
+import temples from '../../assets/OnboardedTemples';
+
+import './Home.css';
+import right from '../../assets/right.svg';
+import left from '../../assets/left.svg';
 
 function Home() {
+
+  const [cardNo, setCardNo] = useState(0);
+
+  const scrollToNext = () => {
+    setCardNo((cardNo + 1) % temples.length)
+  }
+
+  const scrollToPrev = () => {
+    setCardNo((cardNo - 1 + temples.length) % temples.length);
+  }
+
+  useEffect(() => {
+    const slider = document.getElementById('Home__slide');
+    slider.style.transform = `translateX(-${cardNo}00%)`;
+
+  }, [cardNo]);
+
   return (
-    <div>Home</div>
+    <div id='Home'>
+      <section id='Home__vision'>
+        {/* our vision */}
+        <h2>Our Vision</h2>
+        <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. In eius repellat rerum fugit delectus vero sint, animi beatae soluta eveniet consectetur adipisci nihil iure possimus labore ullam et, quidem aperiam?</p>
+      </section>
+      <section id='Home__gallery'>
+        {/* gallery section */}
+        <h2>Gallery</h2>
+        <div id='Home__gallery'>
+          <div id="Home__slide">
+            {
+              temples.map(t => {
+                return <GalleryImg key={t.name} temple={t} />
+              })
+            }
+          </div>
+          <img id='slider-right' onClick={scrollToNext} src={right} alt="" className='slider-img' />
+          <img id='slider-left' onClick={scrollToPrev} src={left} alt="" className='slider-img' />
+          {/* <button onClick={scrollToNext}>Next</button> */}
+        </div>
+        <p>{cardNo + 1}/{temples.length}</p>
+      </section>
+    </div>
+  )
+}
+
+function GalleryImg({ temple }) {
+  return (
+    <div className='slide-component'>
+      <img className='slide-img' src={temple.src} alt={temple.name} />
+      <div className='slide-info'>
+        <h3>{temple.name}</h3>
+        <p id='slide-info__p'>{temple.location}</p>
+      </div>
+    </div>
   )
 }
 
